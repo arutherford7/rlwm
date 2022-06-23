9428b40e624d9bac7ba78213f8bdb82227403ec5
arutherford7 <54595960+arutherford7@users.noreply.github.com>
2022-06-23 15:09:56 -0400

    configmsg

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,12 @@ export const config = {
   min_num_practice_trials: 10,
   max_num_practice_trials: 12,
   practice_correct_threshold: 0.8,
-  num_learn_blocks: 1,
+  num_learn_blocks: 12,
   num_trials_per_learn_block: 4,
   use_design_matrices: true,
   num_bonus_trials: 4,
   randomize_image_set_order: true,
-  enable_db: false,
+  enable_db: true,
   is_debug_db_user: true,
   go_fullscreen: false
 };
