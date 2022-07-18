5c8817d2b154e058338afaa2bdb8832da3e206de
Nick Fagan <nick@fagan.org>
2022-07-18 14:56:50 -0400

    output multiple subjects

--- tool/linearize_database_output.m
+++ tool/linearize_database_output.m
@@ -1,17 +1,32 @@
-js = jsondecode( fileread('C:\Users\nick\Downloads\rlwm-test-default-rtdb-export.json') );
+js = jsondecode( fileread('/Users/nick/Downloads/0705_rlwm-a7d24-default-rtdb-export.json') );
 
 %%
 
-subj_ids = fieldnames( js.meta );
-subj_id = subj_ids{1};
+subj_ids = fieldnames( js.learn_trial_data );
+tbls = cell( size(subj_ids) );
+
+for i = 1:numel(subj_ids)
+
+subj_id = subj_ids{i};
 
 [trial_data, trial_desc] = linearize_trial_data( js.learn_trial_data.(subj_id) );
+meta = extract_meta( js, subj_id );
 
-tbl = to_trial_data_table( trial_data, trial_desc );
+tbls{i} = to_trial_data_table( trial_data, trial_desc, meta );
+
+end
+
+tbl = vertcat( tbls{:} );
+
+%%
+
+if ( 1 )
+  writetable( tbl, '~/Downloads/rtdb-export.csv', 'WriteVariableNames', true );
+end
   
 %%
 
-function t = to_trial_data_table(trial_data, trial_desc)
+function t = to_trial_data_table(trial_data, trial_desc, meta)
 
 block = to_cell( [trial_desc.block_index] );
 block_trial = to_cell( [trial_desc.trial_index] );
@@ -23,13 +38,17 @@ actions = {trial_data.response};
 correct_actions = arrayfun(@(x) {x.correct_response}, [trial_desc.image_descriptor]);
 rt = to_cell( [trial_data.rt] );
 
+dates = repmat( {meta.date}, size(rt) );
+qualtrics_user_ids = repmat( {meta.qualtrics_user_id}, size(rt) );
+subj_ids = repmat( {meta.subject_id}, size(rt) );
+
 m = [ ...
   block(:), block_trial(:), p_high(:), correct(:), rewards(:) ...
-  , actions(:), correct_actions(:), rt(:) ];
+  , actions(:), correct_actions(:), rt(:), dates(:), qualtrics_user_ids(:), subj_ids(:) ];
 
 t = array2table( m, 'VariableNames' ...
   , {'block', 'block_trial', 'p_high_rew', 'corrects', 'rewards' ...
-  , 'actions', 'actionseq', 'rt'});
+  , 'actions', 'actionseq', 'rt', 'date', 'qualtrics_user_id', 'subject_id'});
 
 end
 
@@ -57,5 +76,13 @@ order_by = [ [trial_desc.block_index]', [trial_desc.trial_index]' ];
 [~, perm] = sortrows( order_by );
 trial_data = trial_data(perm);
 trial_desc = trial_desc(perm);
+
+end
+
+function meta = extract_meta(js, subj_id)
+
+meta = js.meta.(subj_id);
+meta = meta.(char(fieldnames(meta)));
+meta.subject_id = subj_id;
 
 end
